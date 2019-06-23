5325ac6dd9b249f2079a4e2fe170d89823801d41
Lindsay Stoner <lindsaybstoner@gmail.com>
2019-06-23 17:30:05 -0400

    get auth0 set up with linkedin

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'dev-qnwo4n3w.auth0.com',
+  clientID: 'lqIZDWtPMwr3B0aSOUH7RVmJL0pUsJOw',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'https://dev-qnwo4n3w.auth0.com/api/v2/'
+}
